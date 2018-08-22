4ddd61c7d6982d4e58127f448983649c5ebce321
Calle Edlund <calle.edlund21@gmail.com>
2018-08-22 05:03:24 +0200

    Still a bug

---
+++ traverseFind.hs
@@ -0,0 +1,19 @@
+import System.Directory(listDirectory, makeAbsolute)
+import Utilities(safeTail)
+import Text.Regex.PCRE
+import System.FilePath.Posix((</>))
+import Control.Monad(forM)
+
+traverseFind :: FilePath -> [String] -> IO [FilePath]
+traverseFind start pats = do abstart <- makeAbsolute start
+                             wins <- (find abstart . breakdown) `mapM` pats
+                             return $ concat wins
+  where breakdown ""      = []
+        breakdown path    = let (a,b) = break (=='/') path
+                            in a : breakdown (safeTail b)
+        find :: FilePath -> [String] -> IO [FilePath]
+        find place (x:xs) = do contents <- listDirectory place
+                               let matches = filter (=~ x) contents
+                               winlist <- forM matches $ (`find` xs) . (place </>)
+                               return $ concat winlist
+        find place _      = return [place]
